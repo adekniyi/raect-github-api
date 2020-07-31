755238f15a3382ea2ead50fa5e09d7b4b5da077c
Adek Niyi <adekniyi@gmail.com>
2020-07-31 05:35:42 -0700

    functionality

--- src/component/Data.js
+++ src/component/Data.js
@@ -1,24 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DataOutput from './DataOutput'
 
 function Data() {
+    const [getValue, setGetValue] = useState("")
+    const [user, getUser] = useState("")
+
+
+
+    const submitForm = (e) => {
+        e.preventDefault()
+        getUser(getValue)
+    }
+
+
     return ( <
         div className = "dataStyle" >
         <
-        form className = "formStyle" >
+        form className = "formStyle"
+        onSubmit = { submitForm } >
         <
         div className = "form-control" >
         <
         input type = "text"
-        placeholder = "type Username" / >
-        <
-        button type = "submit" > Search Github User < /button> <
-        /div> <
+        placeholder = "type Username"
+        value = { getValue }
+        onChange = {
+            (e) => setGetValue(e.target.value)
+        }
+        / > <
+        button type = "submit" > Search Github User < /button> < /
+        div > <
         /
         form > <
-        DataOutput / >
-        <
-        /div>
+        DataOutput user = { user }
+        / > < /
+        div >
     )
 }
 
--- src/component/DataOutput.js
+++ src/component/DataOutput.js
@@ -1,41 +1,75 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import logo from '../logo.svg';
 
 
-function DataOutput() {
+function DataOutput(props) {
+    const [users, setUsers] = useState({})
+
+
+    const user = props.user === "" ? "john" : props.user
+    const client_id = "Iv1.6d532b6c69dc31ae"
+    const client_secret = "b0e3c5671b49920301d88513a7e98ba9e988d24c"
+    const proxy = 'https://cors-anywhere.herokuapp.com/'
+
+
+    useEffect(() => {
+        fetch(`${proxy}https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`)
+            .then((res) => res.json())
+            .then((data) => {
+                setUsers(data)
+            }).catch((err) => console.log(err))
+    }, [users])
+    const { avatar_url, followers, login, name, public_repos, location } = users
     return ( <
         div className = "dataOutput" >
         <
-        img src = { logo }
+        img src = { avatar_url }
         className = "App-logo"
-        alt = "logo" / >
-
-        <
+        alt = "logo" / > <
         h3 style = {
             { paddingBottom: "12px" }
-        } > Adek Niyi < /h3> <
+        } > { login } < /h3> 
+
+        <
         h5 style = {
             { paddingBottom: "5px" }
-        } > Real Name < /h5> <
+        } > Real Name < /h5> 
+
+        <
         p style = {
             { paddingBottom: "12px" }
-        } > Adekunle Adeniyi < /p> <
+        } > { name } < /p> 
+
+        <
         h5 style = {
             { paddingBottom: "5px" }
-        } > Location < /h5> <
+        } > Location < /h5> 
+
+        <
         p style = {
             { paddingBottom: "12px" }
-        } > Lagos Nigeria < /p> <
+        } > { location } < /p> 
+
+        <
         h5 style = {
             { paddingBottom: "5px" }
-        } > Number of Public repo < /h5> <
+        } > Number of Public repo < /h5> 
+
+        <
         p style = {
             { paddingBottom: "12px" }
-        } > 55 < /p> <
+        } > { public_repos } < /p> 
+
+        <
         h5 style = {
             { paddingBottom: "5px" }
-        } > Number of followers < /h5> <
-        p > 119 < /p> < /
+        } > Number of followers < /h5> 
+
+        <
+        p > { followers } < /p> 
+
+        <
+        /
         div >
     )
 }
